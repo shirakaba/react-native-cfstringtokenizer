02fc8fe4eb0a9946d8fd96490e354842fece8d9d
shirakaba <14055146+shirakaba@users.noreply.github.com>
2018-05-18 21:41:12 +0100

    copyBestStringLanguage

--- apple/RCTCFStringTokenizer/RCTCFStringTokenizerBridge.m
+++ apple/RCTCFStringTokenizer/RCTCFStringTokenizerBridge.m
@@ -13,4 +13,10 @@
 
 RCT_EXTERN_METHOD(addEvent:(NSString *)name location:(NSString *)location date:(NSNumber *)date)
 
+// https://stackoverflow.com/a/42874301/5951226 : "Got “is not a recognized Objective-C method” when bridging Swift to React-Native"
+// https://stackoverflow.com/a/24046893/5951226 : unnamed params
+// static methods are represented with + rather than -.
+// https://stackoverflow.com/questions/36309596/swift-ambiguos-methods-with-variadic-paramameter
+RCT_EXTERN_METHOD(copyBestStringLanguage:(NSString *)string :(NSString *)length)
+
 @end
