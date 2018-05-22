cb9dd008d6e3774f0b904e8f4de3a3779ddc28cd
shirakaba <14055146+shirakaba@users.noreply.github.com>
2018-05-22 23:39:50 +0100

    Use method queue.

--- apple/RNCFStringTokenizer/RNCFStringTokenizerBridge.m
+++ apple/RNCFStringTokenizer/RNCFStringTokenizerBridge.m
@@ -6,18 +6,19 @@
 //  Copyright © 2018 Jamie Birch. All rights reserved.
 //
 
+
 #import <Foundation/Foundation.h>
-// #import <React/RCTBridge.h>
 #import <React/RCTBridgeModule.h>
 
 @interface RCT_EXTERN_MODULE(RNCFStringTokenizer, NSObject)
 
-// RCT_EXTERN_METHOD(addEvent:(NSString *)name location:(NSString *)location date:(nonnull NSNumber *)date callback: (RCTResponseSenderBlock)callback);
+// https://facebook.github.io/react-native/docs/native-modules-ios.html#threading
+// We'll run all methods (provisionally) in their own queue to prevent blocking.
+- (dispatch_queue_t)methodQueue
+{
+  return dispatch_queue_create("uk.co.birchlabs.RNCFStringTokenizerQueue", DISPATCH_QUEUE_SERIAL);
+}
 
-// https://stackoverflow.com/a/42874301/5951226 : "Got “is not a recognized Objective-C method” when bridging Swift to React-Native"
-// https://stackoverflow.com/a/24046893/5951226 : unnamed params
-// static methods are represented with + rather than -.
-// https://stackoverflow.com/questions/36309596/swift-ambiguos-methods-with-variadic-paramameter
 RCT_EXTERN_METHOD(copyBestStringLanguage:(NSString *)string :(NSNumber *)length)
 
 @end
